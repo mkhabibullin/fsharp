abe0635f62f5d0da3fe94a78529e12313a5ab3ce
MKhabibullin <safe.kzn@gmail.com>
2016-06-04 17:21:16 +0300

    no message

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,7 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿//Built with F# 1.9.7.8
+open System.IO
+open System.Net
+open Microsoft.FSharp.Control.WebExtensions
+//Add references for the namespaces below if you're not running this code in F# interactive
+open System.Drawing 
+open System.Windows.Forms 
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let buildGoogleChartUri input =
+    let chartWidth, chartHeight = 250,100
+
+    let labels,data = List.unzip input
+    let dataString = 
+        data 
+        |> List.map (box>>string) //in this way, data can be anything for which ToString() turns it into a number
+        |> List.toArray |> String.concat ","
+
+    let labelString = labels |> List.toArray |> String.concat "|"
+
+    sprintf "http://chart.apis.google.com/chart?chs=%dx%d&chd=t:%s&cht=p3&chl=%s"
+            chartWidth chartHeight dataString labelString
+
+//Bake a bitmap from the google chart URI
+let buildGoogleChart myData =
+    async {
+        let req = HttpWebRequest.Create(buildGoogleChartUri myData)
+        let! response = req.AsyncGetResponse()
+        return new Bitmap(response.GetResponseStream())
+    } |> Async.RunSynchronously
+
+//invokes the google chart api to build a chart from the data, and presents the image in a form
+//Excuse the sloppy winforms code
+let test() =
+    let myData = [("John",34);("Sara",30);("Will",20);("Maria",16)]
+
+    let image = buildGoogleChart myData
+    let frm = new Form()
+    frm.BackgroundImage <- image
+    frm.BackgroundImageLayout <- ImageLayout.Center
+    frm.ClientSize <- image.Size
+    frm.Show()
+
+test()
---
+++ ConsoleApplication1/ConsoleApplication2/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open FSharp.Charting
+open System
+open System.Windows.Forms
+
+open System.Drawing
+
+//[<EntryPoint>]
+//[<STAThread>]
+//let main argv = 
+//    printfn "%A" argv
+////    let f = (Chart.Line [ for x in -100 .. 100 -> x, pown x 3]).ShowChart()
+////    System.Windows.Forms.Application.Run(f)
+////    0 // return an integer exit code
+//    
+//    let n = 16
+//    let point i =
+//        let t = float(i % n) / float n * 2.0 * System.Math.PI in (sin t, cos t)
+//    
+//    let f = (Chart.Combine(seq { for i in 0..n-1 do for j in i+1..n -> Chart.Line [point i; point j] })
+//    |> Chart.WithTitle(Text="Artistic FSharp.Charting Sample 1",Color=Color.DarkBlue,Style=ChartTypes.TextStyle.Frame)
+//    |> Chart.WithYAxis(Enabled=false)
+//    |> Chart.WithXAxis(Enabled=false)).ShowChart()
+//    System.Windows.Forms.Application.Run(f)
+//    0
+
+
+open System.Windows.Forms.DataVisualization.Charting
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv = 
+    printfn "%A" argv
+    // Create an instance of chart control and add main area
+    let chart = new Chart(Dock = DockStyle.Fill)
+    let area = new ChartArea("Main")
+    chart.ChartAreas.Add(area)
+
+    // Show the chart control on a top-most form
+    let mainForm = new Form(Visible = true, TopMost = true,  Width = 700, Height = 500)
+    mainForm.Controls.Add(chart)
+    System.Windows.Forms.Application.Run(mainForm)
+    0
